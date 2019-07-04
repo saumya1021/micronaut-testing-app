d48ac3cee6a4e8a3a4496ec849fcfe6da08d40fc
saumya nigam <saumya.may10@gmail.com>
2019-07-04 15:05:08 +0530

    bootstrap files

--- src/main/groovy/com/saumya/controller/HomeController.groovy
+++ src/main/groovy/com/saumya/controller/HomeController.groovy
@@ -5,24 +5,31 @@ import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.views.View
 
+import static io.micronaut.http.HttpResponse.ok
+
 @Controller("/home")
 class HomeController {
 
     @Get("/index")
     @View("index")
     HttpResponse index() {
-        return HttpResponse.ok()
+        return ok()
     }
 
     @Get("/login")
     @View("/login")
     HttpResponse login() {
-        return HttpResponse.ok()
+        return ok()
     }
 
    @Get("/registration")
     @View("/registration")
     HttpResponse registration(){
-       return HttpResponse.ok()
+       return ok()
    }
+    @Get("/grid")
+    @View("/grid")
+    HttpResponse grid(){
+        return ok()
+    }
 }
